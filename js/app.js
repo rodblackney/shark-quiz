c3e8958825a249df0606592b35d67ade73fd2634
Rod Blackney <rblackney@gmail.com>
2015-05-31 18:34:41 +1000

    added another question and updated html

--- js/app.js
+++ js/app.js
@@ -17,5 +17,22 @@ $(document).ready(function() {
 
 		//factoid
         $('#factoid').append('<p>There are more than 350 species of shark - Pygmy Ribbontail Catshark, Prickly Dogfish, and Longnose Sawshark are among them. The Humuhumunukunukuapuaa is the odd one out: Its not a shark - its a tropical triggerfish that gets its name from the pig-like grunting noise it makes.</p>');
-        
+
+ 
+		// question two
+		$('#question').append('<p>The worlds fastest shark hs been recorded at speeds in excess of 50km/h. What species of shark set this records?</p>');
+
+		// choices
+		$('#choice').append('<li>a. Great white shark</li>');
+		$('#choice').append('<li>b. Sicklefin hound shark</li>');
+		$('#choice').append('<li>c. Shortfin mako shark</li>');
+
+		//answer
+		$('#answer-wrong').append('<h3>Your answer was incorrect</h3>');
+		$('#answer-right').append('<h3>Your answer was correct</h3>');
+		$('#answer').append('<h3>The answer is c. Shortfin Mako Shark</h3>');
+
+		//factoid
+		$('#factoid').append('<p>The Shortfin Mako Shark has been clocked at 56km/h (35mph) and can use this impressive speed to launch itself 6 meters out of the water. In short bursts during feeding, its believed they can travel even faster.</p>');
+		
  });
