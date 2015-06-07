8e3e02ba401aefe3ef9d2fe5f0b21b1d0e714c8c
Rod Blackney <rblackney@gmail.com>
2015-06-07 18:57:52 +1000

    updated js

--- js/app.js
+++ js/app.js
@@ -7,10 +7,16 @@ $(document).ready(function() {
     $('#intro').hide();
   });
 
-  $('.submit-answer').on('click', function () {
+  $('.submit-answer').on('click', function (){
     var qname = $(this).data('qname');
     var checkedRadio = $('[name="' + qname + '"]:checked');
     var checkedRadioVal = checkedRadio.val();
+    
+    // if ('.submit-answer' === undefined) {
+    // alert("You'll need to make a choice");
+    // $nextQuestion.disabled();
+  // }
+
     console.log('You answered', checkedRadioVal, 'for', qname);
     var answerDiv = $('.answer', $(this).parent()); // Find the .answer DIV under this BUTTON's parent element
     answerDiv.show();
